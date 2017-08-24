80b1c56cfb27e518d94e965fb12f85b7199ee088
zatm8 <maxis1191@gmail.com>
2017-08-24 23:25:50 +0300

    Fix compiler detection bug
    Signed-off-by: zatm8 <maxis1191@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,6 @@
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, tools
+from conans.errors import ConanException
+from conans.model.version import Version
 import os, shutil
 
 
@@ -12,10 +14,11 @@ class IdealcouscousConan(ConanFile):
     description = "compile-time reflection header-only library for c++1z"
     build_policy = "missing"
     
-    def configure():
-        if self.settings.compiler == "gcc" and self.settings.compiler.version < "7.0":
+    def configure(self):
+        v = Version(str(self.settings.compiler.version))
+        if self.settings.compiler == "gcc" and v < "7.0":
             raise ConanException("GCC >= 7.0 is required")
-        if self.settings.compiler == "clang" and self.settings.compiler.version < "5.0":
+        if self.settings.compiler == "clang" and v < "5.0":
             raise ConanException("clang >= 5.0 is required")
         
 
